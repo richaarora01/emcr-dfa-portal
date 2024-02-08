17eb609a5f3a6ae16c28b10b7109b6a7550c50ea
claudiuvlasceanu <43979009+claudiuvlasceanu@users.noreply.github.com>
2024-02-08 12:57:55 -0800

    update env adn Config page

--- DFAautomationTests/src/main/java/dfa/Config.java
+++ DFAautomationTests/src/main/java/dfa/Config.java
@@ -7,7 +7,7 @@ public class Config {
     //public static final String 		SELECTED_DRIVER = Constants.FIREFOX_DRIVER;
     //public static final String 		SELECTED_DRIVER = Constants.EDGE_DRIVER;
     //public static final String		ENVIROMENT		= Constants.TST;
-    public static final String		ENVIROMENT		= Constants.TST_Support;
+    public static final String		ENVIROMENT		= Constants.TRN_Support;
 
 
     public static String			TEST_SCENARIO						= Constants.NOT_SET;
@@ -16,9 +16,6 @@ public class Config {
     public static final int			TIMEOUT						= 30;							//the time webdriver waits before giving up (in seconds)
     public static final int			SLEEP_TIME_FULL_REFRESH		= 3500;							//time used in Thread.sleep() in milliseconds
     public static final int			SLEEP_TIME_PART_REFRESH		= SLEEP_TIME_FULL_REFRESH/2;	//time used in Thread.sleep() in milliseconds
-
-    //app constants
-    public static BigDecimal CLAIM_CREDITS_PER_AREA 		= new BigDecimal("25");
-    public static String			CLIENT_ID 					= Constants.NOT_SET;
+    
 
 }
